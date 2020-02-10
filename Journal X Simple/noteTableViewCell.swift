2136a588cf2b3a42eb47f7b2047290c525fbba7b
ADM-12 <adm-12@ADM-12s-iMac.local>
2020-02-10 17:42:45 +0530

    TableviewSetup

---
+++ Journal X Simple/noteTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  noteTableViewCell.swift
+//  Journal X Simple
+//
+//  Created by ADM-12 on 10/02/20.
+//  Copyright © 2020 Amogh Joshi. All rights reserved.
+//
+
+import UIKit
+
+class noteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
